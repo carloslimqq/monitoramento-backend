31395cbe569dfad0f29716963a53a90c56729e49
carloslimqq <103339433+carloslimqq@users.noreply.github.com>
2025-11-30 00:01:32 -0300

    main.py

--- main.py
+++ main.py
@@ -1,26 +1,7 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
-from database import Base, engine, get_db
-from models import Leitura
-from mqtt_client import start_mqtt
-
-Base.metadata.create_all(bind=engine)
-
-app = FastAPI()
-
-mqtt_client = start_mqtt()
-
-@app.get("/")
-def raiz():
-    return {"mensagem": "API de Monitoramento Industrial ativa!"}
-
 @app.get("/leituras")
-def listar(maquina: str = None, db: Session = Depends(get_db)):
+def listar(equipamento: str = None, db: Session = Depends(get_db)):
     query = db.query(Leitura)
-    if maquina:
-        query = query.filter(Leitura.maquina == maquina)
+    if equipamento:
+        query = query.filter(Leitura.maquina == equipamento)
     return query.order_by(Leitura.id.desc()).all()
 
-@app.get("/leituras/{id}")
-def buscar(id: int, db: Session = Depends(get_db)):
-    return db.query(Leitura).filter(Leitura.id == id).first()
