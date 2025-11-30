81c1340d76da31eaad68ffb3c5cce009ec0610d5
carloslimqq <103339433+carloslimqq@users.noreply.github.com>
2025-11-29 23:45:00 -0300

    models.py

--- models.py
+++ models.py
@@ -1,12 +1,12 @@
-from sqlalchemy import Column, Integer, String, Float, DateTime
-from sqlalchemy.sql import func
+from sqlalchemy import Column, Integer, String, DateTime, JSON
+from datetime import datetime
 from database import Base
 
-class Telemetria(Base):
-    __tablename__ = "telemetria"
+class Leitura(Base):
+    __tablename__ = "leituras"
 
     id = Column(Integer, primary_key=True, index=True)
     maquina = Column(String(50), index=True)
-    parametro = Column(String(50))
-    valor = Column(Float)
-    timestamp = Column(DateTime(timezone=True), server_default=func.now())
+    dados = Column(JSON)   # Armazena o JSON inteiro
+    timestamp = Column(DateTime, default=datetime.utcnow)
+
